950c944be399a638b18d24ce23f38bea4410f663
enhakkore <ahn.ephraim@gmail.com>
2019-09-16 17:21:02 +0900

    Entity 시간관리 클래스 생성

--- src/main/java/com/enhakkore/webservice/domain/BaseTimeEntity.java
+++ src/main/java/com/enhakkore/webservice/domain/BaseTimeEntity.java
@@ -1,4 +1,23 @@
 package com.enhakkore.webservice.domain;
 
-public class BaseTimeEntity {
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // JPA Entity들이 BaseTimeEntity를 상속할 경우 createData와 modifiedDate 필드들도 칼럼으로 인식함
+@EntityListeners(AuditingEntityListener.class) // BaseTimeEntity에 Auditing 기능을 포함시
+public abstract class BaseTimeEntity {
+
+    @CreatedDate // Entity가 생성되어 저장될 때 시간이 자동으로 저장됨
+    private LocalDateTime createDate;킴
+
+    @LastModifiedDate // 조회한 Entity의 값을 변경할 때 시간이 자동 저장됨
+    private LocalDateTime modifiedDate;
+
 }
