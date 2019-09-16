822641592b4bb40e6017fee83818b9a2dd66b403
enhakkore <ahn.ephraim@gmail.com>
2019-09-16 16:02:11 +0900

    Entity클래스 생성

---
+++ src/main/java/com/enhakkore/webservice/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.enhakkore.webservice.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts { // 실제 DB의 테이블과 매칭될 클래스이며 보통 Entitiy 클래스
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
