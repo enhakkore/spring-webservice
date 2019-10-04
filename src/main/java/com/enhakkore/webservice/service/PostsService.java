43060f0bd7baa956cbdf1f56a758599d1dd206d7
enhakkore <ahn.ephraim@gmail.com>
2019-10-04 21:50:37 +0900

    repository 결과로 넘어온 Posts의 Stream을 map을 통해 PostsMainResponseDto로 변환 -> List로 반환하는 메서드 추가

--- src/main/java/com/enhakkore/webservice/service/PostsService.java
+++ src/main/java/com/enhakkore/webservice/service/PostsService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -15,5 +16,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true) // 조회기능만 남겨두어 조회 속도가 개선
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
