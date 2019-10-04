c04c4f48445e64e73d27be7cdecc527129092c12
enhakkore <ahn.ephraim@gmail.com>
2019-10-04 22:26:09 +0900

    글 목록 기능 구현을 위한 추가 및 수정

--- src/main/java/com/enhakkore/webservice/service/PostsService.java
+++ src/main/java/com/enhakkore/webservice/service/PostsService.java
@@ -1,11 +1,13 @@
 package com.enhakkore.webservice.service;
 
 import com.enhakkore.webservice.domain.posts.PostsRepository;
+import com.enhakkore.webservice.dto.posts.PostsMainResponseDto;
 import com.enhakkore.webservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
