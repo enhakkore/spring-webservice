671e6dc52300173e7d275955294beb8826a70f24
enhakkore <ahn.ephraim@gmail.com>
2019-10-04 21:57:28 +0900

    쿼리 추가

--- src/main/java/com/enhakkore/webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/enhakkore/webservice/domain/posts/PostsRepository.java
@@ -1,7 +1,16 @@
 package com.enhakkore.webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    @Query("SELECT p " +
+            "FROM Posts p " +
+            "ORDER BY p.id DESC")
+    Stream<Posts> findAllDesc();
+    //SpringDataJpa에서 제공하는 기본 메서드만으로 해결할 수 있지만,
+    //SpringDataJpa에서 제공하지 않는 메서드는 위처럼 쿼리로 작성해도 된다는 것을 보여줌
 }
