a6e685128248ac843268ebb6acd2e279305ee835
enhakkore <ahn.ephraim@gmail.com>
2019-10-04 21:48:38 +0900

    응답 전용 Dto 생성

---
+++ src/main/java/com/enhakkore/webservice/dto/posts/PostsMainResponseDto.java
@@ -0,0 +1,31 @@
+package com.enhakkore.webservice.dto.posts;
+
+import com.enhakkore.webservice.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+@Getter
+public class PostsMainResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String modifiedDate;
+
+    //Dto가 Entity를 의존하도록 코드를 작성해야한다.
+    public PostsMainResponseDto(Posts entity){
+        id = entity.getId();
+        title = entity.getTitle();
+        author = entity.getAuthor();
+        modifiedDate = toStringDateTime(entity.getModifiedDate()); // view 영역에서는 LocalDateTime 타입을 모르기 때문에 문자열로 날짜형식을 변경
+    }
+
+    private String toStringDateTime(LocalDateTime localDateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return Optional.ofNullable(localDateTime)
+                .map(formatter::format)
+                .orElse("");
+    }
+}
