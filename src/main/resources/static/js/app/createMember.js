76fa1039bdaa7ac10259e50ee5edba4e8c5a1f5c
enhakkore <ahn.ephraim@gmail.com>
2019-10-14 16:08:58 +0900

    오타 수정

--- src/main/resources/static/js/app/createMember.js
+++ src/main/resources/static/js/app/createMember.js
@@ -17,7 +17,7 @@ var createMember = {
             // dataType: 'json', -> 성공할 때 리턴받을 타입으로 json을 기대,
             // 리턴타입이 json이 아닐 경우 http 요청은 성공하지만 fail이 발생하고 error.status가 200.
             contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
+            data: JSON.stringify(data),
         }).done(function() {
             alert('회원가입이 되었습니다.');
             location.reload();
