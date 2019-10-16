92a9c68c57ce22afaf5ca56a1c736d32c54555bc
enhakkore <ahn.ephraim@gmail.com>
2019-10-16 20:35:06 +0900

    Create WebRestController.java & WebRestControllerTest.java

--- src/main/java/com/enhakkore/webservice/web/WebRestController.java
+++ src/main/java/com/enhakkore/webservice/web/WebRestController.java
@@ -5,10 +5,13 @@ import com.enhakkore.webservice.dto.user.MemberSaveRequestDto;
 import com.enhakkore.webservice.service.PostsService;
 import com.enhakkore.webservice.service.MemberService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
@@ -16,6 +19,7 @@ public class WebRestController {
 
     private PostsService postsService;
     private MemberService memberService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello(){
@@ -30,5 +34,12 @@ public class WebRestController {
     @PostMapping("/createMember")
     public Long createMember(@RequestBody MemberSaveRequestDto dto){
         return memberService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
