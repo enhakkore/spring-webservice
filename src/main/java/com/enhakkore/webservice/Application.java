34d9be8d94ee225e13dce55408a9797d8794b008
enhakkore <ahn.ephraim@gmail.com>
2019-10-16 21:48:56 +0900

    Set to add external application.yml

--- src/main/java/com/enhakkore/webservice/Application.java
+++ src/main/java/com/enhakkore/webservice/Application.java
@@ -1,15 +1,21 @@
 package com.enhakkore.webservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
 
+    public static final String APPLICATION_LOCATION = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/spring-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .profiles(APPLICATION_LOCATION)
+                .run(args);
     }
 
 }
